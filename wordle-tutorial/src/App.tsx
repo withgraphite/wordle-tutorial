cf9c15994473d3af7d498f28111c7c72d7654ee1
Tomas Reimers <tomas@tomasreimers.com>
2022-02-21 11:24:05 -0500

    feat(highlighting): show which letters are correct

--- wordle-tutorial/src/App.tsx
+++ wordle-tutorial/src/App.tsx
@@ -1,9 +1,12 @@
+import classnames from "classnames";
 import { useEffect, useState } from "react";
 import "./App.scss";
 
 const LETTERS = "abcdefghijklmnopqrstuvwxyz";
 
 function App() {
+  // The word the user is trying to guess
+  const [actualWord] = useState("magic");
   // The current word
   const [buffer, setBuffer] = useState("");
   // All previous guesses
@@ -32,13 +35,51 @@ function App() {
   }, [buffer]);
 
   return (
-    <div>
+    <div className="guesses">
       {history.map((pastGuess, idx) => {
-        return <div key={idx}>{pastGuess}</div>;
+        return (
+          <Word key={idx} guess={pastGuess} highlight actualWord={actualWord} />
+        );
       })}
-      <div>{buffer}</div>
+      <Word guess={buffer} actualWord={actualWord} />
     </div>
   );
+}
+
+function Word(props: {
+  guess: string;
+  actualWord: string;
+  highlight?: boolean;
+}) {
+  const guessWithPadding = props.guess.padEnd(5, " ");
+
+  return (
+    <div className="word">
+      {Array.from(guessWithPadding).map((letter, idx) => {
+        return (
+          <Letter
+            key={idx}
+            guess={letter}
+            idx={idx}
+            actualWord={props.actualWord}
+            highlight={props.highlight}
+          />
+        );
+      })}
+    </div>
+  );
+}
+
+function Letter(props: {
+  guess: string;
+  idx: number;
+  actualWord: string;
+  highlight?: boolean;
+}) {
+  return <div className={classnames("letter", {
+    "letter--exactly-correct": props.highlight && props.actualWord[props.idx] === props.guess,
+    "letter--almost-correct": props.highlight && props.actualWord.indexOf(props.guess) !== -1
+  })}>{props.guess}</div>;
 }
 
 export default App;
